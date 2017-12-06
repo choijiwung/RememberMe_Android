1508f95314b9f2eb4e6804f82289e374838f2ac6
choijiwung <784wldnd@naver.com>
2017-12-06 10:05:12 +0900

    Revert "Choi"

--- app/src/main/java/com/rememberme/rememberme/Sign_inActivity.java
+++ app/src/main/java/com/rememberme/rememberme/Sign_inActivity.java
@@ -3,11 +3,11 @@ package com.rememberme.rememberme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class trip_select_Activity extends AppCompatActivity {
+public class Sign_inActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_select_);
+        setContentView(R.layout.activity_sign_in);
     }
 }
